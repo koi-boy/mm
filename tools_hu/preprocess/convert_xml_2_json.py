f9b37361ce24bb2e8ce13c1daad304d2778a2e5a
huker <hukeroy@163.com>
2019-12-30 17:08:23 +0800

    ADD

--- tools_hu/preprocess/convert_xml_2_json.py
+++ tools_hu/preprocess/convert_xml_2_json.py
@@ -90,16 +90,16 @@ def convert(name_list, xml_dir, save_json, code_dictionary):
 
 
 if __name__ == '__main__':
-    file_dir = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\train_test_dataset\bottom\train'
+    file_dir = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\images'
     name_lst = []
     for root, _, files in os.walk(file_dir):
         for file in files:
             if file.endswith('jpg') or file.endswith('JPG'):
                 name_lst.append(file[:-4])
 
-    json_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\train_test_dataset\bottom\train.json'
-    code_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\train_test_dataset\bottom\classes.txt'
-    id_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\train_test_dataset\bottom\id.txt'
+    json_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\all.json'
+    code_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\classes.txt'
+    id_file = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\id.txt'
     code = CodeDictionary(code_file, id_file)
 
     convert(name_lst, file_dir, json_file, code)
--- tools_hu/preprocess/train_test_split.py
+++ tools_hu/preprocess/train_test_split.py
@@ -1,28 +1,30 @@
 import shutil
 import os
 import random
+import glob
 
 def train_test_split(img_dir, xml_dir, train_size, output):
-    imgs = os.listdir(img_dir)
+    imgs = glob.glob(os.path.join(img_dir, '*.jpg'))
     train_set = random.sample(imgs, int(len(imgs)*train_size))
     train_dir = os.path.join(output, 'train')
-    test_dir = os.path.join(output, 'test')
+    test_dir = os.path.join(output, 'val')
     if not os.path.exists(train_dir):
         os.makedirs(train_dir)
     if not os.path.exists(test_dir):
         os.makedirs(test_dir)
     for img in imgs:
-        xml = img[:-4] + '.xml'
+        img_name = img.replace('\\', '/').split('/')[-1]
+        xml = img_name[:-4] + '.xml'
         if img in train_set:
-            shutil.copy(os.path.join(img_dir, img), os.path.join(train_dir, img))
+            shutil.copy(img, os.path.join(train_dir, img_name))
             shutil.copy(os.path.join(xml_dir, xml), os.path.join(train_dir, xml))
         else:
-            shutil.copy(os.path.join(img_dir, img), os.path.join(test_dir, img))
+            shutil.copy(img, os.path.join(test_dir, img_name))
             shutil.copy(os.path.join(xml_dir, xml), os.path.join(test_dir, xml))
 
 if __name__ == '__main__':
-    img_dir = r'D:\Project\WHTM\data\21101\final_dataset\images'
-    anns_dir = r'D:\Project\WHTM\data\21101\final_dataset\annotations'
+    img_dir = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\images'
+    anns_dir = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom\images'
     TRAIN_SIZE = 0.9
-    output = r'D:\Project\WHTM\data\21101\train_test'
+    output = r'D:\Project\chongqing_contest\data\chongqing1_round1_train1_20191223\bottom'
     train_test_split(img_dir, anns_dir, TRAIN_SIZE, output)
